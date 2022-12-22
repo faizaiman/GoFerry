7e5350b62b362758a06338f3dabdbdca929a4253
ahmxdrif <ahmadarifhazman@gmail.com>
2022-12-23 02:48:32 +0800

    tinggal buat formpage

--- lib/pages/displayFerry.dart
+++ lib/pages/displayFerry.dart
@@ -1,10 +1,11 @@
-import 'package: flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goferry/common_widgets/ferryBuilder.dart';
 import 'package:goferry/models/ferryticket.dart';
 import 'package:goferry/models/user.dart';
 import 'package:goferry/services/ferryservice.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class DisplayPage extends StatefulWidget {
   const DisplayPage({Key? key}) : super(key: key);
@@ -14,9 +15,13 @@ class DisplayPage extends StatefulWidget {
 
 class _DisplayPageState extends State<DisplayPage> {
   final DatabaseService _databaseService = DatabaseService();
-  ////////
-  ////////
-  ///////
+
+  Future<void> _onFerryTicketDelete(FerryTicket ferryTicket) async
+  {
+    await _databaseService.deleteFerryTicket(ferryTicket.book_id);
+    setState((){});
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -38,9 +43,48 @@ class _DisplayPageState extends State<DisplayPage> {
         (
           children:
           [
-            FerryBuilder(future: _databaseService.getFerryTickets(), onDelete: , onEdit: onEdit)
+            // FerryBuilder
+            // (
+            //   future: _databaseService.getFerryTickets(), 
+            //   onDelete: _onFerryTicketDelete, 
+            //   onEdit: (value)
+            //   {
+            //     Navigator.of(context).push
+            //     (
+            //       MaterialPageRoute
+            //       (
+            //         builder: (_) => BrandFormPage(ferryTicket:value),
+            //         fullscreenDialog: true,
+            //       ),
+            //     ).then((_)=>setState((){}));
+            //   }
+            // ),
+          ],
+        ),
+        floatingActionButton: Column
+        (
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: 
+          [
+            FloatingActionButton
+            (
+              onPressed: ()
+              {
+                Navigator.of(context).push
+                (
+                  MaterialPageRoute
+                  (
+                    builder: (context) => const BrandFormPage(),
+                    fullscreenDialog: true,
+                  )
+                ).then((_)=> setState((){}));
+              },
+              heroTag: 'addFerryTicket',
+              child: const FaIcon(FontAwesomeIcons.plus),
+            ),
+            const SizedBox(height: 12.0),
           ]
-        )
+        ),
       ),
     );
   }
