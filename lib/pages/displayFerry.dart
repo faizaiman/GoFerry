227b22b89a40fad32e7a4de23a3e3a90033be921
Faizznddn <59620015+faizaiman@users.noreply.github.com>
2023-01-02 22:47:05 +0800

    uhuk

--- lib/pages/displayFerry.dart
+++ lib/pages/displayFerry.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:goferry/common_widgets/ferryBuilder.dart';
 import 'package:goferry/models/ferryticket.dart';
 import 'package:goferry/models/user.dart';
